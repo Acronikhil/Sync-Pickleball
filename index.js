8f17d54d0c156af630533ed43e8ee8aa40c1045a
Acronikhil <20.nikhildubey@gmail.com>
2025-07-12 23:22:23 +0530

    mobile responsive

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleCalendar() {
+    document.getElementById("calendarOverlay").classList.remove("hidden");
+  }
+  
+  function closeCalendar() {
+    document.getElementById("calendarOverlay").classList.add("hidden");
+  }
+  
